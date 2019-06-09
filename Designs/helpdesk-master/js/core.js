5782a47d269f1edb4b65bf9ee2cbe66c3cda51b5
Muhitdin Yahya <monkeydjack0@mail.ru>
2019-06-10 00:22:08 +0200

    index page

---
+++ Designs/helpdesk-master/js/core.js
@@ -0,0 +1,47 @@
+$(document).ready(function(){
+
+	$('.menu-btn').click(function(){		
+			$('.menu').slideToggle(700);	
+			if ($(this).hasClass('not-active')) {
+				$(this).addClass('is-active').removeClass('not-active');
+			}else{
+				setTimeout(function(){
+					$('.is-active').addClass('not-active').removeClass('is-active')
+				},600)			
+			}		
+		});
+$('.slider').slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  dots: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false
+      }
+    }
+  ]
+});
+
+});
