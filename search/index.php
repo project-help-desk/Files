f92222f45b106fd378101409ac697047e19d6b46
jonathanmohamed <jonathanmohamed34@gmail.com>
2019-06-30 23:47:56 +0200

    search function, check read me

---
+++ search/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title>Search</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+</head>
+<body>
+ <form action="./search.php" method="POST">
+    <input type="text" name="query" />
+    <input type="submit" value="Search" />
+</form>
+</body>
+</html>
