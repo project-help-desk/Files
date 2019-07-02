8b024ef1fc3df286c2e17ae50418a0d7b597e2e5
Thomas Koops <thomas_koops@hotmail.com>
2019-07-02 12:49:30 +0200

    Added things

---
+++ helpdesk-master/OverviewReports.php
@@ -0,0 +1,60 @@
+<?php
+
+$currentDate = date('Y-m-d');
+$WeekDate = date('Y-m-d', strtotime('-7 days'));
+$MonthDate = date('Y-m-d', strtotime('-30 days'));
+$YearDate = date('Y-m-d', strtotime('-365 days'));
+$conn = mysqli_connect("localhost", "root", "", "stenden_helpdesk");
+$weekquery = "SELECT count(incident_id) 
+FROM incident
+WHERE
+date_time BETWEEN '" . $WeekDate . "' AND '" . $currentDate . "'";
+if ($statement = mysqli_prepare($conn, $weekquery)) {
+    if (mysqli_stmt_execute($statement)) {
+        mysqli_stmt_bind_result($statement, $weekcount);
+        mysqli_stmt_store_result($statement);
+        mysqli_stmt_fetch($statement);
+    } else {
+        echo "error inserting incident";
+        die(mysqli_error($conn));
+    }
+    mysqli_stmt_close($statement);
+} else {
+    die(mysqli_error($conn));
+}
+
+$monthquery = "SELECT count(incident_id) 
+FROM incident
+WHERE
+date_time BETWEEN '" . $MonthDate . "' AND '" . $currentDate . "'";
+if ($statement = mysqli_prepare($conn, $monthquery)) {
+    if (mysqli_stmt_execute($statement)) {
+        mysqli_stmt_bind_result($statement, $monthcount);
+        mysqli_stmt_store_result($statement);
+        mysqli_stmt_fetch($statement);
+    } else {
+        echo "error inserting incident";
+        die(mysqli_error($conn));
+    }
+    mysqli_stmt_close($statement);
+} else {
+    die(mysqli_error($conn));
+}
+$yearquery = "SELECT count(incident_id) 
+FROM incident
+WHERE
+date_time BETWEEN '" . $YearDate . "' AND '" . $currentDate . "'";
+if ($statement = mysqli_prepare($conn, $yearquery)) {
+    if (mysqli_stmt_execute($statement)) {
+        mysqli_stmt_bind_result($statement, $yearcount);
+        mysqli_stmt_store_result($statement);
+        mysqli_stmt_fetch($statement); 
+    } else {
+        echo "error inserting incident";
+        die(mysqli_error($conn));
+    }
+    mysqli_stmt_close($statement);
+} else {
+    die(mysqli_error($conn));
+}
+?>
