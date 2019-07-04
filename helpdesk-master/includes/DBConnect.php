d68e3fecdf1cf7fe7339959bdaffc740479c7558
Thomas Koops <thomas_koops@hotmail.com>
2019-07-04 13:48:04 +0200

    DBConnect

--- helpdesk-master/includes/DBConnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$conn = mysqli_connect("localhost", "root", "", "stenden_helpdesk");
-if (!$conn) {
-    echo "Connection to the server  not established";
-}
-?>
--- helpdesk-master/includes/dbh-inc.php
+++ helpdesk-master/includes/dbh-inc.php
@@ -1,24 +1,8 @@
 <?php
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "stenden_helpdesk";
-
-$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
-
-//Check your connection === FALSE
-
-if ($conn == FALSE) {
+$conn = mysqli_connect("localhost", "root", "", "stenden_helpdesk");
+if (!$conn) {
     echo "<p>Unable to connect to the database server.</p>"
-            . "<p>Error code " . mysqli_errno($conn) . ": " . mysqli_error($conn)
-            . "</p>";
+    . "<p>Error code " . mysqli_errno($conn) . ": " . mysqli_error($conn)
+    . "</p>";
 }
-
-
-
-
-
-
-
-
 ?>
