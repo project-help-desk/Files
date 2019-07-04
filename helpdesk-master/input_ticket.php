23afba636ded06a2ebf797384097e9896f9bafd5
Ian Donker <47434563+Akolix@users.noreply.github.com>
2019-07-04 14:38:47 +0200

    Added a redirect to mytickets.php after submitting.

--- helpdesk-master/input_ticket.php
+++ helpdesk-master/input_ticket.php
@@ -51,7 +51,8 @@ and open the template in the editor.
                                 //Binds variables to a prepared statement as parameters
                                 mysqli_stmt_bind_param($statement, "ssii", $date, $desc, $issue, $contact_id);
                                 if (mysqli_stmt_execute($statement)) {
-                                    echo "Incident inserted successfully";
+                                    echo "Incident inserted successfully, you will be redirected to your tickets.";
+                                    header("Refresh: 5; URL=UserTickets.php");
                                 } else {
                                     echo "error inserting incident";
                                     die(mysqli_error($conn));
